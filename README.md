be3d2128b168f543cc51ae9658d179db5a473f15
Nick Byrne <nick@digitalnative.net>
2023-10-04 13:24:27 +0100

    working app with documentation on how to use

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
+# Keycloak Tokens
 
-# Get an Access token from Keycloak API
+Get an Access token from Keycloak API
 
     ADMIN_NAME=admin
     ADMIN_PASSWORD=password
@@ -28,8 +29,41 @@ Set the access token variable for future use
 
     ACCESS_TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2l...."
 
+If you see no output then remove the pipe to jq amd you see no output then
+remove the jq arg and check the error message.
 
-# Get a user
+# Setup Keycloak
+
+We are creating client for testing and getting JWT tokens.
+
+* Go to http://localhost:8180/auth/
+* Login with admin/password
+* Go to Clients section on sidebar
+* Click Create button
+* Fill Client ID as 'login-app'
+* Click Save button
+
+# Testing the App
+
+Without a session token
+
+    curl http://localhost:8280/users/me/items/
+    {"detail":"Not authenticated"}
+
+With an expired or invalid session token
+
+    curl http://localhost:8280/users/me/items/  --header "Authorization: Bearer ${ACCESS_TOKEN}"
+    {"detail":"Could not validate credentials"}
+    
+With a valid session token as shown above, set CLIENT_ID to login-app
+
+    curl http://localhost:8280/users/me/items/ --header "Authorization: Bearer ${ACCESS_TOKEN}"
+    [{"item_id":"Foo","owner":"admin"}]
+    
+
+# Keycloak Admin Usage
+
+## Get a user
 
 Once the access token is set, to get details about a user use 
 
@@ -49,7 +83,7 @@ or to list all users in the realm use
 
 Again piping the output to jq makes it easier to read. 
 
-# Delete a user
+## Delete a user
 
 From the Get a user section, obtain the value from the user 'id' field.
 
@@ -63,6 +97,6 @@ From the Get a user section, obtain the value from the user 'id' field.
 Nothing is return apart from 200 OK so to verify its deleted run the Get a user
 request again.
 
-# Creat a user
+## Create a user 
 
-This is not required as creation is performed via Active Directory.
+TODO
--- app/app/main.py
+++ app/app/main.py
@@ -1,6 +1,51 @@
-from fastapi import FastAPI
+import sys
+import os
+from typing import Optional
 
+import requests
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from jose.constants import ALGORITHMS
+from pydantic import BaseModel, BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "Test Keycloak Auth App"
+    public_key: Optional[str] = None
+    keycloak_url = os.getenv("KEYCLOAK_URL", "http://localhost:8180")
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class User(BaseModel):
+    username: str
+
+settings = Settings()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
+
+@app.on_event("startup")
+def read_kc_public_key():
+    # Why doing this?
+    # Because we want to fetch public key on start
+    # Later we would verify incoming JWT tokens
+    try:
+        r = requests.get(settings.keycloak_url + "/auth/realms/master", timeout=3)
+        r.raise_for_status()
+        response_json = r.json()
+        settings.public_key = f'-----BEGIN PUBLIC KEY-----\r\n{response_json["public_key"]}\r\n-----END PUBLIC KEY-----'
+    except requests.exceptions.HTTPError as errh:
+        print("Http Error:", errh)
+        sys.exit(1)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+        sys.exit(1)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+        sys.exit(1)
+    except requests.exceptions.RequestException as err:
+        print("OOps: Something Else", err)
+        sys.exit(1)
 
 
 @app.get("/")
@@ -12,3 +57,31 @@ def read_root():
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.public_key, algorithms=[ALGORITHMS.RS256],
+                             options={"verify_signature": True, "verify_aud": False, "exp": True})
+        username: str = payload.get("preferred_username")
+        token_data = TokenData(username=username)
+    except JWTError as e:
+        print(e)
+        raise credentials_exception
+    return token_data
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
